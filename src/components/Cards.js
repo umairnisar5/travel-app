eaa8cff1c12edfba21b8d1abb576d20181a96cf2
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-22 17:41:43 +0500

    update Cards.js

---
+++ src/components/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Cards.css';
+import Img9 from '../images/img-9.jpg';
+import Img2 from '../images/img-2.jpg';
+import Img3 from '../images/img-3.jpg';
+import Img4 from '../images/img-4.jpg';
+import Img8 from '../images/img-8.jpg';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src={Img9}
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/'
+            />
+            <CardItem
+              src={Img2}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src={Img3}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/'
+            />
+            <CardItem
+              src={Img4}
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/'
+            />
+            <CardItem
+              src={Img8}
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
